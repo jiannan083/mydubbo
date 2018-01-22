bdd33d9ca0792083789ef0e7b1ed3550e3b922ea
wangjiannan <wangjiannan@nan.com>
2018-01-22 11:27:58 +0800

    client test

--- mydubbo-client/src/test/java/cn/wangjiannan/test/UserTest.java
+++ mydubbo-client/src/test/java/cn/wangjiannan/test/UserTest.java
@@ -5,30 +5,38 @@ import java.util.List;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import cn.wangjiannan.model.User;
-import cn.wangjiannan.model.vo.UserVo;
-import cn.wangjiannan.service.UserService;
+import cn.wangjiannan.mydubbo.model.User;
+import cn.wangjiannan.mydubbo.service.UserService;
 import cn.wangjiannan.test.base.BaseTest;
 
 public class UserTest extends BaseTest {
 	@Autowired
 	private UserService userService;
 
-	@Test
-	public void testUser() {
-		UserVo userVo = new UserVo();
-		userVo.setLoginName("admin");
-		// userVo.setId(1L);
-		List<User> list = userService.selectByLoginName(userVo);
-		logger.info("-----" + list.get(0).toString());
-	}
+	// @Test
+	// public void testUser() {
+	// UserVo userVo = new UserVo();
+	// userVo.setLoginName("admin");
+	// // userVo.setId(1L);
+	// List<User> list = userService.selectByLoginName(userVo);
+	// logger.info("-----" + list.get(0).toString());
+	// }
+	//
+	// @Test
+	// public void testUserPage() {
+	// // PageVo<User> pageVo = new PageVo<>(1, 10, "id", "asc");
+	// // Map<String, Object> condition = new HashMap<String, Object>();
+	// // pageVo.setCondition(condition);
+	// // userService.selectUserPage(pageVo);
+	// // System.out.println("-----------");
+	// }
 
 	@Test
-	public void testUserPage() {
-		// PageVo<User> pageVo = new PageVo<>(1, 10, "id", "asc");
-		// Map<String, Object> condition = new HashMap<String, Object>();
-		// pageVo.setCondition(condition);
-		// userService.selectUserPage(pageVo);
-		// System.out.println("-----------");
+	public void testUser1() {
+		User user = new User();
+		user.setLoginName("admin");
+		// userVo.setId(1L);
+		List<User> list = userService.selectByLoginName(user);
+		logger.info("-----" + list.get(0).toString());
 	}
 }
